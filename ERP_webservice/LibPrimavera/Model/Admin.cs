ec37bf230d0990d05c076297318936cfcf57cc28
jpguimaraes <j.pedro.guimaraes@hotmail.com>
2013-12-07 22:32:43 +0000

    Admin model

---
+++ ERP_webservice/LibPrimavera/Model/Admin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstREST.Lib_Primavera.Model
+{
+    public class Admin
+    {
+        public string CodAdmin
+        {
+            get;
+            set;
+        }
+        public string NomeAdmin
+        {
+            get;
+            set;
+        }
+        public string NumContribuinte
+        {
+            get;
+            set;
+        }
+        public string Moeda
+        {
+            get;
+            set;
+        }
+    }
+
+}
